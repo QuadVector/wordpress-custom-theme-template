7e5970148b2d29ca198b09b539a950cc11b9a7b4
QuadVector Software <66649825+QuadVector@users.noreply.github.com>
2024-04-05 01:58:41 +0300

    Changed order of required scripts

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 add_action("init", function () {
     if (!is_admin() && $GLOBALS["pagenow"] != "wp-login.php") {
-        wp_enqueue_style("style-main", get_stylesheet_directory_uri() . "/css/main.css");
         wp_enqueue_style("style-bootstrap", get_stylesheet_directory_uri() . "/css/bootstrap.min.css");
+        wp_enqueue_style("style-main", get_stylesheet_directory_uri() . "/css/main.css");
 
         wp_enqueue_script("jquery");
-        wp_enqueue_script("script-main", get_stylesheet_directory_uri() . "/js/main.js", 100);
         wp_enqueue_script("script-bootstrap", get_stylesheet_directory_uri() . "/js/bootstrap.min.js", 100);
+        wp_enqueue_script("script-main", get_stylesheet_directory_uri() . "/js/main.js", 100);
     }
 
     register_nav_menus(array(
