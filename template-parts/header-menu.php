f0fb86c51441273cf87793762b4e01e2673737c0
QuadVector Software <66649825+QuadVector@users.noreply.github.com>
2024-05-12 18:47:28 +0300

    Included bootstrap navwalker

--- template-parts/header-menu.php
+++ template-parts/header-menu.php
@@ -5,7 +5,7 @@
         "container"         => "div",
         "container_class"   => "nav navbar-nav top-nav",
         "container_id"      => "menu",
-        "menu_class"        => "nav navbar-nav"
+        "menu_class"        => "nav navbar-nav",
         "fallback_cb"       => "WP_Bootstrap_Navwalker::fallback",
         "walker"            => new WP_Bootstrap_Navwalker(),
     ));
